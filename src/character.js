323285f8edd07c950d5a43d4545eafb85f436a5f
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-01-13 16:38:04 -0600

    Added Buttons & Basic Animation

--- src/character.js
+++ src/character.js
@@ -1,33 +1,59 @@
+import * as React from 'react';
+
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Slide from '@mui/material/Slide';
 
 import InitIcon from '@mui/icons-material/Bolt';
 import ACIcon from '@mui/icons-material/Shield';
 import HPIcon from '@mui/icons-material/Favorite';
 import StatusIcon from '@mui/icons-material/Flare';
-import EnlargeIcon from '@mui/icons-material/UnfoldMore';
-import ReduceIcon from '@mui/icons-material/UnfoldLess';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoveIcon from '@mui/icons-material/UnfoldMore';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from '@mui/icons-material/Delete';
 
+
+const extraButtons = (
+  <Stack direction="row" alignItems="bottom"  spacing={1}>
+    <IconButton>
+      <MoveIcon />
+    </IconButton>
+
+    <IconButton>
+      <DeleteIcon />
+    </IconButton>
+  </Stack>
+);
 
 export default function BasicGrid() {
-  return (
-    <Box sx={{ flexGrow: 1, m: 2}}>
-      <Grid container spacing={1}>
 
-        <Grid item xs={2.75}>
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+
+  return (
+    <Box sx={{ ml: 1, mr: 1, mt: 1}}>
+      <Stack direction="row" alignItems="bottom" spacing={1} sx={{ ml:1, mr:1, mb: -1 }}>
+        <Grid container spacing={1}>
+
+          <Grid item xs={2.75}>
             <TextField
             placeholder="Name"
             fullWidth
             variant = "standard"
             autoComplete="off"
             />
-        </Grid>
+          </Grid>
 
-        <Grid item xs={1.25}>
+          <Grid item xs={1.25}>
             <TextField
             placeholder="Init"
             fullWidth
@@ -41,9 +67,9 @@ export default function BasicGrid() {
               ),
             }}
             /> 
-        </Grid>
+          </Grid>
 
-        <Grid item xs={1.25}>
+          <Grid item xs={1.25}>
             <TextField
             placeholder="AC"
             fullWidth
@@ -57,9 +83,9 @@ export default function BasicGrid() {
               ),
             }}
             />
-        </Grid>
+          </Grid>
 
-        <Grid item xs={1.75}>
+          <Grid item xs={1.75}>
             <TextField
             placeholder="HP"
             fullWidth
@@ -73,25 +99,39 @@ export default function BasicGrid() {
               ),
             }}
             />
-        </Grid>
+          </Grid>
 
-        <Grid item xs={5}>
-            <TextField
-            placeholder="Status"
-            fullWidth
-            variant = "standard"
-            autoComplete="off"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <StatusIcon />
-                </InputAdornment>
-              ),
-            }}
-            />
-        </Grid>
+          <Grid item xs={5}>
+            <Stack direction="row" alignItems="bottom" spacing={1}>
 
-      </Grid>
+              <TextField
+              placeholder="Status"
+              fullWidth
+              variant = "standard"
+              autoComplete="off"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <StatusIcon />
+                  </InputAdornment>
+                ),
+              }}
+              />
+
+              <IconButton onClick={handleChange}>
+                <MoreIcon />
+              </IconButton>
+            
+              <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
+                {extraButtons}
+              </Slide>
+
+            </Stack>
+          </Grid>
+
+        </Grid>
+      </Stack>
+
       <Divider sx={{ mt: 1, mb: 1 }}/>
     </Box>
   );
