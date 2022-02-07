593993d4016cef877e6c782263d0bcd437cfa9b4
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-02-07 03:55:43 -0600

    Added top and bottom App Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import CharacterList from './CharacterList';
 import './App.css';
 
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+
 function App() {
   return (
     <div className="App">
+      <AppBar position="sticky" color="primary" sx={{ top: 0, bottom: 'auto' }}>
+        <Toolbar>
+        </Toolbar>
+      </AppBar>
+      
       <CharacterList />
+
+      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+        </Toolbar>
+      </AppBar>
+
     </div>
   );
 }
