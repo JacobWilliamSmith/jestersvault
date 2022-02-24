05168bd80cc8a2916fd34ab112359b5ddf525b5e
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-02-23 18:24:44 -0600

    Auto-scrolling for create character action

--- src/CharacterList.js
+++ src/CharacterList.js
@@ -13,11 +13,22 @@ export default function CharacterList() {
   const dispatch = useDispatch();
   const characters = useSelector(state => state.characters);
 
+  function handleCreateCharacter() {
+    const createCharacterPromise = () => new Promise((resolve, reject) => {
+      dispatch(createCharacter());
+      resolve();
+    })
+
+    createCharacterPromise().then(() => {
+      window.scroll({ top: document.body.offsetHeight, left: 0,  behavior: 'smooth' });
+    })
+  }
+
   return (
     <div>
       <CharacterListHeader />
       { characters.map((c) => ( <Character key={c.id} id={c.id} /> ))}
-      <Button fullWidth sx={{m:0, p:0}} onClick={ () => { dispatch(createCharacter()) }}>
+      <Button fullWidth sx={{m:0, p:0}} onClick={ () => {handleCreateCharacter()} }>
         <h3>ADD CHARACTER</h3>
       </Button>
     </div>
