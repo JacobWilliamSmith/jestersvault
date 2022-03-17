94fc26f3f781c76988c92e55d54cf6527a49ce10
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-03-16 22:42:02 -0500

    Added login dialog (not yet functional) to client

--- client/src/AppBarMenu.js
+++ client/src/AppBarMenu.js
@@ -1,20 +1,30 @@
 import { styled } from "@mui/material/styles";
 
+import * as React from 'react';
+
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
+import Button from '@mui/material/Button';
 
 import MenuIcon from '@mui/icons-material/Menu';
 
 import DrawerMenu from './DrawerMenu';
+import AuthDialog from './AuthDialog';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleDrawer } from './actions';
 
 export default function AppBarMenu() {
+  const [isAuthOpen, setIsAuthOpen] = React.useState(false);
+
+  const handleToggleAuth = () => {
+    setIsAuthOpen(!isAuthOpen);
+  };
+
   const dispatch = useDispatch();
   const isDrawerOpen = useSelector(state => state.isDrawerOpen)
   const drawerWidth = useSelector(state => state.drawerWidth)
@@ -50,9 +60,14 @@ export default function AppBarMenu() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             Jester's Vault
           </Typography>
+          <Button color="inherit" onClick={handleToggleAuth} size="large" >
+            Login
+          </Button>
+          <AuthDialog isOpen={isAuthOpen} onClose={handleToggleAuth} />
+
         </Toolbar>
       </CustomAppBar>
       <DrawerMenu />
---
+++ client/src/AuthDialog.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 1 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default function AuthDialog(props) {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <div>
+      <Dialog open={props.isOpen} onClose={props.onClose}>
+        <Box sx={{bgcolor: 'background.paper', width: 500 }}>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+            >
+              <Tab label="Log In"/>
+              <Tab label="Create Account"/>
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  label="Username"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="dense"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={props.onClose}>Cancel</Button>
+                <Button onClick={props.onClose}>Log in</Button>
+              </DialogActions>
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+            <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  label="Username"
+                  type="text"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="dense"
+                  label="Email"
+                  type="email"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="dense"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  variant="standard"
+                />
+                <TextField
+                  margin="dense"
+                  label="Confirm Password"
+                  type="password"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={props.onClose}>Cancel</Button>
+                <Button onClick={props.onClose}>Create Account</Button>
+              </DialogActions>
+            </TabPanel>
+          </SwipeableViews>
+        </Box>
+            
+      </Dialog>
+    </div>
+  );
+}
