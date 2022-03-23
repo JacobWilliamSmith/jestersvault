cb5fc8a2648d5792711f3e702aa7ab8d2f12c1a1
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-03-23 02:13:06 -0500

    LoginForm is connected to backend

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,24 +1,63 @@
-import * as React from 'react';
+import React, {useState, useContext} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import Box from '@mui/material/Box';
+import AuthContext from '../contexts/Auth';
+import AuthService from '../services/Auth';
 
 export default function AuthDialog(props) {
-  const [login, setLogin] = React.useState({username: "", password: ""});
+  const [login, setLogin] = useState({username: "", password: ""});
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
+
+  const onChange = e => {
+    e.preventDefault();
+    setLogin({...login,[e.target.name] : e.target.value});
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    AuthService.login(login).then(data=>{
+      const { isAuthenticated, user, message } = data;
+      if(isAuthenticated) {
+        authContext.setUser(user);
+        authContext.setIsAuthenticated(isAuthenticated);
+        props.onClose();
+      } else {
+        setMessage(message);
+        console.log(message);
+      }
+    })
+  }
 
   return (
     <Box sx={{ p: 1 }}>
+      <form>
       <DialogContent>
-        <TextField margin="dense" variant="standard" fullWidth label="Username" type="text" autoFocus />
-        <TextField margin="dense" variant="standard" fullWidth label="Password" type="password" />
-      </DialogContent>
+        <TextField name="username"
+                  label="Username"
+                  type="text"
+                  autoFocus
+                  onChange={onChange}
+                  margin="dense"
+                  variant="standard"
+                  fullWidth />
+        <TextField name="password"
+                  label="Password"
+                  type="password"
+                  onChange={onChange}
+                  margin="dense"
+                  variant="standard"
+                  fullWidth />
+        </DialogContent>
 
-      <DialogActions>
-        <Button onClick={props.onClose}>Cancel</Button>
-        <Button onClick={props.onClose}>Log in</Button>
-      </DialogActions>
+        <DialogActions>
+          <Button onClick={props.onClose}>Cancel</Button>
+          <Button onClick={onSubmit}>Log in</Button>
+        </DialogActions>
+      </form>
     </Box>  
   );
 }
