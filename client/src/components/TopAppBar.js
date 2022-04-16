dcf380221718ed22d79035bcf197ff798a9d235f
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-04-15 19:11:13 -0500

    Added nonfunctional settings & bookmark button

--- client/src/components/TopAppBar.js
+++ client/src/components/TopAppBar.js
@@ -3,9 +3,16 @@ import {useState, useContext} from 'react';
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography';
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+
+import SettingsIcon from '@mui/icons-material/Settings';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import { AuthContext } from '../contexts/Auth';
 import AuthService from '../services/Auth';
@@ -14,22 +21,6 @@ import AuthDialog from './AuthDialog';
 export default function AppBarMenu() {
   const [isAuthOpen, setIsAuthOpen] = useState(false);
   const {isAuthenticated, setIsAuthenticated, setUser} = useContext(AuthContext);
-
-  const loginButton = () => {
-    return (
-      <Button color="inherit" onClick={handleToggleAuth} size="large" >
-        Log In
-      </Button>
-    )
-  }
-
-  const logoutButton = () => {
-    return (
-      <Button color="inherit" onClick={logout} size="large" >
-        Log Out
-      </Button>
-    )
-  }
 
   const logout = () => {
     AuthService.logout().then(data=>{
@@ -52,10 +43,39 @@ export default function AppBarMenu() {
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             Jester's Vault
           </Typography>
-          { isAuthenticated ? logoutButton() : loginButton() }
-          <AuthDialog isOpen={isAuthOpen} onClose={handleToggleAuth} />
+          <Stack direction="row" alignItems="flex-end" spacing={1}>
+            { isAuthenticated &&
+              <Tooltip title="Bookmarked Characters / Games">
+                <IconButton color="inherit">
+                  <BookmarkIcon />
+                </IconButton>
+              </Tooltip>
+            }
+            
+            <Tooltip title="Settings">
+              <IconButton color="inherit">
+                <SettingsIcon />
+              </IconButton>
+            </Tooltip>
+            
+            { isAuthenticated
+              ? <Tooltip title="Log Out">
+                  <IconButton onClick={logout} color="inherit">
+                    <LogoutIcon />
+                  </IconButton>
+                </Tooltip>
+
+              : <Tooltip title="Log in">
+                  <IconButton onClick={handleToggleAuth} color="inherit">
+                    <LoginIcon />
+                  </IconButton>
+                </Tooltip>
+            }
+
+          </Stack>
         </Toolbar>
       </AppBar>
+      <AuthDialog isOpen={isAuthOpen} onClose={handleToggleAuth} />
     </Box>
   )
 }
