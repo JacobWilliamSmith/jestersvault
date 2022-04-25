ef9cbb2dc4edf208bc6aa33b47987f99d01064cb
Jacob Smith <too.many.jacob.smiths@gmail.com>
2022-04-25 04:39:51 -0500

    Minor refactoring

--- client/src/components/Character.js
+++ client/src/components/Character.js
@@ -95,7 +95,6 @@ export default function Character(props) {
               >
                 {character.name.charAt(0).toUpperCase()}
               </Avatar>
-              
             </div>
           }
   
